4f8fc1a12bf707bcc1c26072390c0fc71268d98b
Jake Wilson <jake.e.wilson@gmail.com>
2022-11-08 16:56:20 -0700

    Added basic sandbox config webview

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import { config } from 'process';
 import * as vscode from 'vscode';
+import { readFile } from 'fs/promises';
+import { addSandbox } from './webViews';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -10,19 +13,31 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vscode-b2cc-content-editor" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('b2cc-content-editor.addSandbox', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Add a sandbox!');
+		// vscode.window.showInformationMessage('Add a sandbox!');
+		// config.b2ccContentEditor.asset.visible = true;
+		const panel = vscode.window.createWebviewPanel(
+			'sandboxConfiguration',
+			'Sandbox',
+			vscode.ViewColumn.One,
+			{}
+		);
+
+		panel.webview.html = addSandbox();
 	});
-
-
 
 	context.subscriptions.push(disposable);
 }
+
+// let obj: webViews = {};
+
+// async function getAddSandboxWebviewContent() {
+// 	if (!webViews.addSandbox.html) {
+// 		webViews.addSandbox.html = (await readFile('./webViews/addSandbox/addSandbox.html')).toString();
+// 	}
+
+// 	return webViews.addSandbox.html;
+// }
 
 // This method is called when your extension is deactivated
 export function deactivate() { }
